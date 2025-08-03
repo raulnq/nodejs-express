fd7c4807cfa48056a718fd73bc018109d133c77e
Raul Naupari <raulnq@gmail.com>
2025-08-02 22:55:22 -0500

    feat: migration added

---
+++ knexfile.js
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+const config = {
+  development: {
+    client: 'pg',
+    connection: process.env.CONNECTION_STRING,
+    migrations: {
+      directory: './migrations',
+      extension: 'js',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+  },
+};
+
+export default config;
