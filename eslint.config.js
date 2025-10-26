a96fcf3b1b362b2235cdd1880eb049412121d228
Raul Naupari <raulnq@gmail.com>
2025-10-26 10:04:31 -0500

    fix: eslint fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,17 @@
 import js from '@eslint/js';
 import globals from 'globals';
 import { defineConfig } from 'eslint/config';
-import prettier from 'eslint-plugin-prettier';
 import prettierConfig from 'eslint-config-prettier';
 
 export default defineConfig([
+  js.configs.recommended,
+  prettierConfig,
   {
     files: ['**/*.{js,mjs,cjs}'],
-    plugins: {
-      js,
-      prettier,
-    },
-    extends: ['js/recommended'],
     languageOptions: {
       globals: {
         ...globals.node,
       },
-    },
-    rules: {
-      ...prettierConfig.rules,
-      'prettier/prettier': ['error', { endOfLine: 'crlf' }],
     },
   },
 ]);
